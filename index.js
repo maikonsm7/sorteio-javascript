746bd5b1f59d99d8f3c65b4d712a54c3713b97f4
Família Monteiro <familiamonteiro@192.168.100.2>
2023-12-16 12:23:14 -0400

    add comentario

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
+// funcao arrow que retorna o numero aleatorio gerado
 const rand = (min = 0, max = 100) => Math.floor(Math.random() * (max - min) + min)
+// imprimir numero aleatorio
 console.log(rand(10, 15))
